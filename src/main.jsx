6076ac234f16beb878d22f7ea65d63b02db67fb0
Douglas <portogdouglas@gmail.com>
2023-10-10 13:48:17 -0300

    criando Tema (theme.js)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
+import {ThemeProvider} from "styled-components"
+import theme from './styles/theme'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 )
