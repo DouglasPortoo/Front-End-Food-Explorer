04608c45f0911081ab00572687458a3114d2f35c
Douglas <portogdouglas@gmail.com>
2023-10-10 15:31:22 -0300

    Criação do Stepper

---
+++ src/components/Stepper/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+background-color: none;
+color: ${({ theme }) => theme.COLORS.Light_300};
+
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 14px;
+
+/* width: 100px;
+height: 32px; */
+
+`
