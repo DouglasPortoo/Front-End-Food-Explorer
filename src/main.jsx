5b3f953937a58b00d83bee94a6f511a9f8824cad
Douglas <portogdouglas@gmail.com>
2023-10-10 14:10:31 -0300

    configurando o Estilo Global

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
-import {ThemeProvider} from "styled-components"
-import theme from './styles/theme'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./pages/Home";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <Home />
+      <GlobalStyles />
+      <Home />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
