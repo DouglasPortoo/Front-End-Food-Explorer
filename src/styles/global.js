efe868af36d3cd91dcb3ff6b0f6cf30fc56da69c
Douglas <portogdouglas@gmail.com>
2023-10-10 14:25:00 -0300

    Personalizando a fonte

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,18 @@
-import {createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
 export default createGlobalStyle`
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  :root{
+    font-size: 16px;
+
+    @media (max-width:768px) {
+      font-size: 12px;
+    }
   }
 
   body {
@@ -15,8 +23,9 @@ export default createGlobalStyle`
   }
 
   body, input, button, textarea {
-    font-family: 'Roboto Slab', serif;
-    font-size: 16px;
+    /* font-family: 'Poppins', sans-serif; */
+    font-family: 'Roboto Slab', serif; 
+    font-size: 1rem;
     outline: none;
   }
 
