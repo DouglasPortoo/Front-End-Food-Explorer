ed085d40c9d9bd367a50f4f14c67c5106b205c40
Douglas <portogdouglas@gmail.com>
2023-10-15 15:22:31 -0300

    Adicionando a pagina 404

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
+import { Prato } from '../pages/Prato'
 import { AdicionarPrato } from '../pages/AdicionarPrato'
 import { EditarPrato } from '../pages/EditarPrato'
-import { Prato } from '../pages/Prato'
+import { NotFound} from '../pages/NotFound'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Prato/:id" element={<Prato />} />
       <Route path="/AdicionarPrato" element={<AdicionarPrato />} />
       <Route path="/EditarPrato/:id" element={<EditarPrato />} />
-      <Route path="/Prato/:id" element={<Prato />} />
+
+      <Route path="*" exact={true} element={<NotFound/>} />
   
     </Routes>
   )
