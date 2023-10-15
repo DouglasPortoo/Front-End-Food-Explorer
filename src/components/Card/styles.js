cac8a4433aa6d924e6032c5c65f12d6ce8a1bfa1
Douglas <portogdouglas@gmail.com>
2023-10-15 16:29:20 -0300

    adicionando animações no Card

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 width: 304px;
-/* height: 28rem; */
 background-color: ${({ theme }) => theme.COLORS.Dark_200};
 
 border-radius: 8px;
@@ -14,6 +13,27 @@ flex-direction: column;
 align-items: center;
 text-align: center;
 gap: 0.9rem;
+
+span{
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 160%;
+
+  color: ${({ theme }) => theme.COLORS.Cake_100};
+
+}
+
+cursor: pointer;
+transition: all .2s ease-in-out;
+
+&:hover{
+
+-webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+-moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+transition: all 0.4s ease 0s;
+transform: scale(1.1);
+}
 
 >img{
   width: 11rem;
@@ -27,12 +47,22 @@ display: flex;
 gap:1rem;
 `
 
-export const FavoriteButton= styled.button`
+export const FavoriteButton = styled.button`
 position: relative;
 left: 120px;
 cursor: pointer;
+transition: all .2s ease-in-out;
 
 background: none;
 border: none;
 color:white;
+
+&:hover{
+
+-webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+-moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+transition: all 0.4s ease 0s;
+transform: scale(1.1);
+}
 `
